d1a4b177977643ca511a3df03e660133bfdaddfa
Rikigyy919 <102380457+Rikigyy919@users.noreply.github.com>
2022-04-14 03:20:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
    * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Contact Me on wa.me/6287733172047
    * Follow https://github.com/DikaArdnt
 */
 
@@ -18,14 +18,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281575886399','6281575886399','6281575886399']
-global.premium = ['6281575886399']
-global.ownernomer = '6281575886399'
+global.owner = ['6287733172047','6287733172047','6287733172047']
+global.premium = ['6287733172047']
+global.ownernomer = '6287733172047'
 global.ownername = 'RiychDwayne'
 global.botname = 'Chika-Md'
 global.footer = 'RiychDwayne'
-global.ig = 'https://instagram.com/riychdwayne'
-global.email = 'riychh.20@gmail.com'
+global.ig = 'https://instagram.com/rixs650'
+global.email = 'ikyjb683@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://api-riychdwayne.herokuap.com'
